09366774fa8b7b44bc92a791ce563d4d30b5332a
0xThresh <0xthresh@protonmail.com>
2022-06-25 17:49:01 -0400

    convention fixes

--- Chat.sol
+++ Chat.sol
@@ -3,28 +3,28 @@ pragma solidity ^0.8.14;
 
 contract Unichat {
     // variables
-    uint256 FixedPaymentAmount = 1000000000000000000; // 1 eth in wei
+    uint256 fixedPaymentAmount = 1000000000000000; // .001 eth in wei
     mapping(string => address) public submitters; 
     mapping(address => uint256) public balances;
 
     // events
     event NewEntry(string _hash);  
 
-    // this needs to be executed before AddData() can be called
-    function FundContract() payable public returns(bool) {
-        require(msg.value == FixedPaymentAmount); 
+    // this needs to be executed before addData() can be called
+    function fundContract() payable public returns(bool) {
+        require(msg.value == fixedPaymentAmount); 
         submitters['initial'] = msg.sender;
         return true;
     }
         
 
-    function AddData(string memory _oldHash, string memory _newHash) payable public returns(bool) {         
-        require((msg.value) >= FixedPaymentAmount, "Not enough ETH sent to stake.");
+    function addData(string memory _oldHash, string memory _newHash) payable public returns(bool) {         
+        require((msg.value) >= fixedPaymentAmount, "Not enough ETH sent to stake.");
         require(bytes20(submitters[_oldHash]) != 0, "Another hash has been submitted by another node.");
         
         
         // receive ETH to be staked 
-        (bool storeSuccess,) = msg.sender.call{value: FixedPaymentAmount}("");
+        (bool storeSuccess,) = msg.sender.call{value: fixedPaymentAmount}("");
         require(storeSuccess, "Transfer IN failed.");
 
         
@@ -33,7 +33,7 @@ contract Unichat {
 
 
         // add the freed up balance to the balances map 
-        balances[submitters[_oldHash]] += FixedPaymentAmount;
+        balances[submitters[_oldHash]] += fixedPaymentAmount;
 
         // clean up submitters[_oldHash]]
         delete submitters[_oldHash];
