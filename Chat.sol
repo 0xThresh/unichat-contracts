c11c2296c49c48d0479d289984c1a0dd48bd0fdf
0xThresh.eth <104535511+0xThresh@users.noreply.github.com>
2022-06-25 07:01:47 -0400

    convention updates

--- Chat.sol
+++ Chat.sol
@@ -1,30 +1,29 @@
-// SPDX-License-Identifier: no
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14; 
 
 contract Unichat {
     // variables
-    uint256 fixedPaymentAmount = 1000000000000000000; // 1 eth in wei
+    uint256 FixedPaymentAmount = 1000000000000000000; // 1 eth in wei
     mapping(string => address) public submitters; 
     mapping(address => uint256) public balances;
 
     // events
-    event newMessage(string _hash);  
+    event NewEntry(string _hash);  
 
-    // this needs to be executed before addData() can be called
-    function fundContract() payable public returns(bool) {
-        require(msg.value == fixedPaymentAmount); 
+    // this needs to be executed before AddData() can be called
+    function FundContract() payable public returns(bool) {
+        require(msg.value == FixedPaymentAmount); 
         submitters['initial'] = msg.sender;
         return true;
     }
         
 
-    function addData(string memory _oldHash, string memory _newHash) payable public returns(bool) {         
-        // require that the sender included the required amount of eth
-        require((msg.value) >= fixedPaymentAmount, "Not enough ETH sent to stake.");
+    function AddData(string memory _oldHash, string memory _newHash) payable public returns(bool) {         
+        require((msg.value) >= FixedPaymentAmount, "Not enough ETH sent to stake.");
         
         
         // receive ETH to be staked 
-        (bool storeSuccess,) = msg.sender.call{value: fixedPaymentAmount}("");
+        (bool storeSuccess,) = msg.sender.call{value: FixedPaymentAmount}("");
         require(storeSuccess, "Transfer IN failed.");
 
         
@@ -32,28 +31,28 @@ contract Unichat {
         submitters[_newHash] = msg.sender;
 
 
-        // add the freed up balances to the balances map 
-        balances[submitters[_oldHash]] += fixedPaymentAmount;
+        // add the freed up balance to the balances map 
+        balances[submitters[_oldHash]] += FixedPaymentAmount;
 
         // clean up submitters[_oldHash]]
         delete submitters[_oldHash];
 
-        emit newMessage(_newHash);
+        emit NewEntry(_newHash);
 
         return true; 
     }
 
 
-    function withdrawFunds(address payable _address) external {
+    function WithdrawFunds(address payable _address) external returns(bool) {
         // sends back ETH to anyone whose funds have been freed up after another address submits hashes
         uint256 currentBalance;
-        
-        //locked = true;
         currentBalance = balances[_address];
         balances[_address] = 0;
        (bool success,) =    
         _address.call{value: currentBalance}('');
         
         require(success, "Transfer OUT failed.");
+    
+        return true;
     }
 }
